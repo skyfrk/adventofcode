7e849b30e0cdde7bca4a644f6b23cc300070d569
skyfrk <github@esslinger.dev>
2020-12-07 14:19:30 +0100

    feat(2020): implement day 7 part 1

--- src/AdventOfCode.2020.Day07/Program.cs
+++ src/AdventOfCode.2020.Day07/Program.cs
@@ -1,3 +1,30 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
-var input = File.ReadAllText("input.txt");
+var input = File.ReadAllLines("input.txt");
+
+var bagColorRegex = new Regex(@"^(\w+ \w+)");
+var propertiesRegex = new Regex(@"(\d+) (\w+ \w+)");
+
+Dictionary<string, (int Count, string Color)[]> allBags = new();
+
+foreach (var line in input)
+{
+    var color = bagColorRegex.Match(line).Groups[1].Value;
+    var props = propertiesRegex.Matches(line).Select(m => (int.Parse(m.Groups[1].Value), m.Groups[2].Value)).ToArray();
+    allBags.Add(color, props);
+}
+
+string[] GetBagsThatCanContainOne(string color)
+{
+    var bagsDirect = allBags.Where(b => b.Value.Any(r => r.Color == color && r.Count >= 1));
+    var bagsIndirect = bagsDirect.SelectMany(b => GetBagsThatCanContainOne(b.Key));
+
+    return bagsDirect.Select(b => b.Key).Concat(bagsIndirect).Distinct().ToArray();
+}
+
+Console.WriteLine($"Part 1: {GetBagsThatCanContainOne("shiny gold").Length}");
+    
