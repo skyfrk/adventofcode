4cd1426d5f04baf4ac9689209bb63088c84e4906
skyfrk <github@esslinger.dev>
2020-12-09 12:19:28 +0100

    feat(2020): implement day 9 part 1

--- src/AdventOfCode.2020.Day09/Program.cs
+++ src/AdventOfCode.2020.Day09/Program.cs
@@ -1,6 +1,13 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
-var input = File.ReadAllLines("input.txt");
+var input = File.ReadAllLines("input.txt").Select(line => long.Parse(line)).ToArray();
+
+for (int i = 25; i < input.Length; i++)
+{
+    var prevNums = input[(i - 25)..i];
+    if (prevNums.SelectMany(num => prevNums.Where(x => x != num).Select(num2 => num2 + num)).Contains(input[i])) continue;
+    Console.WriteLine($"Part 1: {input[i]}");
+    break;
+}
