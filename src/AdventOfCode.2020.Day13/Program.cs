9de7e06e5315bb1ab79f31653b47f3e62103429c
github@esslinger.dev <simon@esslinger.dev>
2020-12-13 12:40:53 +0100

    feat(2020): implement day 13 part 1

--- src/AdventOfCode.2020.Day13/Program.cs
+++ src/AdventOfCode.2020.Day13/Program.cs
@@ -1,5 +1,25 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
 
 var input = File.ReadAllLines("input.txt");
+
+var earliestLeaveTimestamp = int.Parse(input[0]);
+
+var busIdRegex = new Regex(@"(\d)+");
+
+var busIds = busIdRegex.Matches(input[1]).Select(m => int.Parse(m.Value));
+
+for(int i = earliestLeaveTimestamp; true; i++)
+{
+    foreach (var busId in busIds)
+    {
+        if(i % busId == 0)
+        {
+            var waitTime = i - earliestLeaveTimestamp;
+            Console.WriteLine($"Part 1: {waitTime * busId}");
+            Environment.Exit(0);
+        }
+    }
+}
