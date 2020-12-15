23dae7ba0f1891abbceff0f63b3fc3c26e557f10
github@esslinger.dev <simon@esslinger.dev>
2020-12-15 20:21:32 +0100

    feat(2020): implement day 15 part 2

--- src/AdventOfCode.2020.Day15/Program.cs
+++ src/AdventOfCode.2020.Day15/Program.cs
@@ -1,26 +1,62 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 
 var input = File.ReadAllLines("input.txt")[0].Split(',').Select(int.Parse).ToArray();
 
-var targetNumber = 2020;
-var spokenNumbers = input.ToList();
-
-for(int i = input.Length; i < targetNumber; i++)
+void SolvePart1()
 {
-    var prevNum = spokenNumbers[i - 1];
+    var targetNumber = 2020;
+    var spokenNumbers = input.ToList();
 
-    var lastIdx = spokenNumbers.SkipLast(1).ToList().LastIndexOf(prevNum);
+    for (int i = input.Length; i < targetNumber; i++)
+    {
+        var prevNum = spokenNumbers[i - 1];
 
-    if(lastIdx == -1)
-    {
-        spokenNumbers.Add(0);
+        var lastIdx = spokenNumbers.SkipLast(1).ToList().LastIndexOf(prevNum);
+
+        if (lastIdx == -1)
+        {
+            spokenNumbers.Add(0);
+        }
+        else
+        {
+            spokenNumbers.Add(i - 1 - lastIdx);
+        }
     }
-    else
-    {
-        spokenNumbers.Add(i - 1 - lastIdx);
-    }
+
+    Console.WriteLine($"Part 1: {spokenNumbers[targetNumber - 1]}");
 }
 
-Console.WriteLine($"Part 1: {spokenNumbers[targetNumber - 1]}");
+void SolvePart2()
+{
+    var targetNumber = 30000000;
+    var memory = new Dictionary<int, int>();
+
+    // seed memory
+    for (int i = 0; i < input.Length - 1; i++) memory.Add(input[i], i);
+
+    // start with last number from seed
+    int number = input[input.Length - 1];
+
+    for(int i = input.Length; i < targetNumber; i++)
+    {
+        if (!memory.TryGetValue(number, out var value))
+        {
+            memory[number] = i - 1;
+            number = 0;
+        }
+        else
+        {
+            int newNumber = i - 1 - value;
+            memory[number] = i - 1;
+            number = newNumber;
+        }
+    }
+
+    Console.WriteLine($"Part 2: {number}");
+}
+
+SolvePart1();
+SolvePart2();
